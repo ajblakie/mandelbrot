5c24b942438443c984b870d2714251f082c17e2a
A J Blakie <ajblakie@tutanota.com>
2021-12-06 14:52:11 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,23 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn complex_square_add_loop(c: Complex<f64>) {
+/// Try to determine if 'c' is in the mandelbrot set, using at most 'limit'
+/// iterations to decide.
+///
+/// If 'c' is not a member, return 'Some(i)', where 'i' is the number of
+/// iterations it took for 'c' to leave the circle of radius 2 centred on the 
+/// origin. If 'c' seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that 'c' is not a member),
+/// return 'None'
+fn escape_time(c: Complex<f64> limit: usize)-> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
         z = z * z + c;
     }
+
+    None;
 }
 
